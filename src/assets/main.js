29aed740ad3e06df1e5c93ab74ec8e89eb1e9dd1
Xenock20 <juliosaboredo@gmail.com>
2022-09-04 19:45:14 -0300

    Integracion de la Pagina de practica para API

---
+++ src/assets/main.js
@@ -0,0 +1,40 @@
+const API = "https://youtube-v31.p.rapidapi.com/search?channelId=UCw_GCCpbyMrj_axkGyacJcA&part=snippet%2Cid&order=date&maxResults=10"
+const content = null || document.getElementById('content')
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c68edbdba9mshc3cb6eb8038f3b2p1f444ajsna5de19fe1256',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi){
+  const res = await fetch(urlApi, options)
+  const data = await res.json()
+  return data
+}
+
+(async () => {
+  try{
+    const videos = await fetchData(API)
+    let view = `
+    ${videos.items.map(video => `
+    <div class="group relative">
+            <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+              <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full" />
+            </div>
+            <div class="mt-4 flex justify-between">
+              <h3 class="text-sm text-gray-700">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                ${video.snippet.title}
+              </h3>
+            </div>
+          </div>
+    `).slice(0,4).join('')}     
+    `;
+    content.innerHTML = view;
+  }catch (error){
+    console.log(error)
+  }
+})();
